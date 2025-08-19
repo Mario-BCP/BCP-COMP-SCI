6e09bad276a5fb17fd98600d300313cfa165834c
Mario-BCP <mariomirandabanderas@gmail.com>
2025-08-19 17:29:45 -0400

    Day 1 Random.py

--- Day 1 Random.py
+++ Day 1 Random.py
@@ -1,11 +1,18 @@
 import random
-
-def choose_player(players):
-    if not players: 
-        return now    # in case the array is empty
-    return random.choice(players)
+# lets us use like the random function options
+def choose_player(players): 
+    #Define a function that takes a list of players
+    if not players:
+         #check if the list is empty
+        return None    
+    # if its empty, return None (no player to choose)
+    return random.choice(players) 
+# pick and return one random player from the list
 
 # Example usage: 
-players = [ "lebron", "curry", "Jordan", "Kobe", "KD", "Poole", "Tatum", "SGA"]
-chosen = choose_player(players)
-print("chosen player:", chosen)
+players = [ "lebron", "curry", "Jordan", "Kobe", "KD", "Poole", "Tatum", "SGA"] 
+ #creates like the list
+chosen = choose_player(players)  
+#CHooses one player
+print("chosen player:", chosen) 
+#shows the chosen player
